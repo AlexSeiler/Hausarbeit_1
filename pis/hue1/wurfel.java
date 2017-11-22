afcde0f07c364d78dc7a5c4988ad93902c14aecd
AlexSeiler <alex74598@googlemail.com>
2017-11-22 02:09:30 +0100

    Erste idee zum Codieren

--- pis/hue1/wurfel.java
+++ pis/hue1/wurfel.java
@@ -4,4 +4,18 @@ package pis.hue1;
  * Created by Alex on 22.11.2017.
  */
 public class wurfel {
+    /**
+     * fülle ein array mit dem Schlüsselwort und durchlaufe es mit dem Alphabet verteile dabei
+     * eine Zahl an die gewünschte stelle (evtl zwei diminsionen?) BSP:
+     * int x =0;
+     * Wald -> ary[] = [W][a][l][d]
+     * for (i=0;i< alphabet.length,i++)
+     *      for (j=0;j< ary.length,j++)
+     *          if alphabet[i] == ary[j] (W = W )
+     *              ary[j] = x (=0)
+     *              x+=1
+     *
+     *
+     *
+     */
 }
