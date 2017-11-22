3aaea7dffe5ce2de951b7039ddfb7637b76ed9a0
AlexSeiler <alex74598@googlemail.com>
2017-11-23 00:42:16 +0100

    Methode kodieren implementiert

--- pis/hue1/wurfel.java
+++ pis/hue1/wurfel.java
@@ -1,6 +1,7 @@
 package pis.hue1;
 
 import java.io.*;
+import java.lang.StringBuilder;
 import java.util.Arrays;
 
 /**
@@ -8,48 +9,69 @@ import java.util.Arrays;
  */
 public class wurfel {
     public static void main(String args[]) {
-        System.out.print(Zahlenlosung("Hallo"));
+        System.out.print(Kodieren("Schwarzwald","Schwenningen","eintreffendersendungverspaetetneuerterminfolgt"));
     }
     private static String Zahlenlosung(String losung){
-        char[] losungswort = losung.toCharArray();
         int counter =0;
         int[] x= new int[losung.length()];
         for(char alphabet = 'A'; alphabet <= 'Z';alphabet++) {
             for(int i=0;i< losung.length();i++){
-                if (alphabet == Character.toLowerCase(losungswort[i])){
+                if (alphabet == Character.toLowerCase(losung.charAt(i))){
                     x[i] = counter;
                     counter +=1;}
-                if (alphabet == Character.toUpperCase(losungswort[i])){
+                if (alphabet == Character.toUpperCase(losung.charAt(i))){
                     x[i] = counter;
                     counter +=1;}
             }
         }
         return Arrays.toString(x);
     }
-    /**
-     * Methode "Zahlenlosung"
-     *
-     * fülle ein array mit dem Schlüsselwort, durchlaufe es für jeden buchstaben im Alphabet, bei einem match setze
-     * den jetzigen counter an der stelle und erhöhe ihn um 1 BSP:
-     * int counter =0;
-     * Wald -> ary[] = [W][a][l][d]
-     * for (i=0;i< alphabet.length,i++)
-     *      for (j=0;j< ary.length,j++)
-     *          if alphabet[i] == ary[j] (W = W )
-     *              ary[j] = counter (=0)
-     *              counter+=1
-     *
-     *Methode "Kodieren"
-     *
-     * eine schleife mit dem array ary wird durchlaufen solange es noch buchstaben eines zu kodierenden textes gibt
-     * ary sollte nun [3][0][2][1] so aussehen
-     *
-     * for (int i =0; i< textzukodieren.length;i++)
-     *      if ary position = ary.length
-     *          ary position = 0
-     *      buchstabe aus text = ary[position]add buchstabe
-     *      ary+=1
-     * ary[0].inhalt +ary[1] +ary[2] +ary[3] = kodierter text
-     *
-     */
+
+    private static String Kodieren(String losung1,String losung2, String text){
+        String umstand1 = Zahlenlosung(losung1);
+        String umstand2 = Zahlenlosung(losung2);
+        String[] schluessel1 = umstand1.substring(1,umstand1.length()-1).split(", ");
+        String[] schluessel2= umstand2.substring(1,umstand2.length()-1).split(", ");
+        String[] kodiert1=new String [schluessel1.length];
+        String[] kodiert2=new String [schluessel2.length];
+        String einfachwurfel ="";
+        String zweifachwurfel="";
+        int x=0;
+        int y=0;
+        for(int i=0;i< text.length();i++ ){
+            if (x==schluessel1.length){
+                x=0;
+                y=1;}
+                if (y==1) {
+                    kodiert1[Integer.parseInt(schluessel1[x])] += text.charAt(i);
+                    x += 1;
+                 }
+                else{
+                    kodiert1[Integer.parseInt(schluessel1[x])] ="";
+                    kodiert1[Integer.parseInt(schluessel1[x])] += text.charAt(i);
+                    x+=1;}
+        }
+        for(int k =0;k< kodiert1.length;k++){
+            einfachwurfel+=kodiert1[k];
+        }
+        x=0;
+        y=0;
+        for(int i=0;i< einfachwurfel.length();i++ ){
+            if (x==schluessel2.length){
+                x=0;
+                y=1;}
+                if (y==1) {
+                    kodiert2[Integer.parseInt(schluessel2[x])] += einfachwurfel.charAt(i);
+                    x += 1;
+                }
+                else{
+                    kodiert2[Integer.parseInt(schluessel2[x])] ="";
+                    kodiert2[Integer.parseInt(schluessel2[x])] += einfachwurfel.charAt(i);
+                    x+=1;}
+        }
+        for(int l=0;l<kodiert2.length;l++){
+            zweifachwurfel+=kodiert2[l];
+        }
+        return zweifachwurfel;
+    }
 }
